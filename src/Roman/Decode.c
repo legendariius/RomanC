4a22c415b813d543033e4bc6499890f150c68e44
Louis Cheinisse <louis.cheinisse@yahoo.fr>
2015-03-13 17:27:51 +0100

    TP2 : ecriture de tests avec cppunit

--- src/Roman/Decode.c
+++ src/Roman/Decode.c
@@ -1,5 +1,31 @@
 #include "Decode.h"
+#include <string.h>
 
-int roman_decode(char* in) {
-  return 42;
+int roman_decode(char in[]) {
+	int i;
+	for (int i = 0; i < strlen(in); ++i) { 
+	  switch(in[0]) {
+	  	case 'I':
+	  		return 1;
+	  		break;
+	  	case 'V':
+	  		return 5;
+	  		break;
+	  	case 'X':
+	  		return 10;
+	  		break;
+	  	case 'L':
+	  		return 50;
+	  		break;
+	  	case 'C':
+	  		return 100;
+	  		break;
+	  	case 'D':
+	  		return 500;
+	  		break;
+	  	case 'M':
+	  		return 1000;
+	  		break;
+	  }
+	}
 }
--- tests/Roman/DecodeTest.cpp
+++ tests/Roman/DecodeTest.cpp
@@ -21,3 +21,26 @@ TEST(Roman, Decode_I)
   CHECK_EQUAL(1, roman_decode("I"));
 }
 
+TEST (Roman, Decode_V) {
+	CHECK_QUAL(5, roman_decode("V"));
+}
+
+TEST (Roman, Decode_X) {
+	CHECK_EQUAL(10, roman_decode("X"));
+}
+
+TEST (Roman, Decode_L) {
+	CHECK_EQUAL(50, roman_decode("L"));
+}
+
+TEST (Roman, Decode_C) {
+	CHECK_EQUAL(100, roman_decode("C"));
+}
+
+TEST (Roman, Decode_D) {
+	CHECK_EQUAL(500, roman_decode("D"));
+}
+
+TEST (Roman, Decode_M) {
+	CHECK_EQUAL(1000, roman_decode("M"));
+}
